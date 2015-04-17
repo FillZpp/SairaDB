9b63cec39896427fe85afa96c7529fb074dc7b15
Siyu Wang <FillZpp.pub@gmail.com>
2015-04-17 18:02:05 +0800

    add args and readline

---
+++ client/src/sr_args.rs
@@ -0,0 +1,80 @@
+// SairaDB - A distributed database
+// Copyright (C) 2015 by Siyu Wang
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+extern crate crypto;
+extern crate getopts;
+extern crate libc;
+
+use std::io::{stderr, Write};
+use std::collections::HashMap;
+use std::env;
+use self::getopts::Options;
+use self::crypto::digest::Digest;
+use self::crypto::md5::Md5;
+
+
+pub fn get_flags() -> HashMap<String, String> {
+    let mut flag_map = HashMap::new();
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this help menu");
+    opts.optopt("", "cookie", "Set cookie for connection safety", "COOKIE");
+    opts.optopt("", "ip", "Master IP", "IP");
+    opts.optopt("", "port", "Port master listened for client", "PORT");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            let _ = writeln!(&mut stderr(), "Error:\nArgs parse error:\n{}", f);
+            unsafe { libc::exit(3); }
+        }
+    };
+
+    if matches.opt_present("h") {
+        usage(opts);
+        unsafe { libc::exit(0); }
+    }
+
+    flag_map.insert("cookie".to_string(), {
+        let ck = match matches.opt_str("cookie") {
+            Some(a) => a,
+            None => "".to_string()
+        };
+        let mut cal = Md5::new();
+        cal.input_str(&ck);
+        cal.result_str().to_string()
+    });
+
+    flag_map.insert("ip".to_string(), match matches.opt_str("ip") {
+        Some(a) => a,
+        None => "127.0.0.1".to_string()
+    });
+
+    flag_map.insert("port".to_string(), match matches.opt_str("port") {
+        Some(a) => a,
+        None => "4400".to_string()
+    });
+
+    flag_map
+}
+
+fn usage(opts: Options) {
+    print!("{}", opts.usage("Usage: saira-slave [OPTIONS]"));
+}
+
---
+++ client/src/sr_readline.rs
@@ -0,0 +1,60 @@
+// SairaDB - A distributed database
+// Copyright (C) 2015 by Siyu Wang
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+use std::ffi::{CStr, CString};
+use std::str::from_utf8;
+use std::io::stdin;
+
+#[cfg(unix)]
+#[link(name = "readline")]
+extern "C" {
+    #[link_name = "readline"]
+    fn rl_readline(prompt: *const i8) -> *const i8;
+}
+
+#[cfg(unix)]
+fn read_line_unix(prompt: &str) -> Option<String> {
+    let pr = CString::new(prompt.as_bytes()).unwrap();
+    let sp = unsafe { rl_readline(pr.as_ptr()) };
+
+    if sp.is_null() {
+        None
+    } else {
+        let cs = unsafe { CStr::from_ptr(sp) };
+        Some(from_utf8(cs.to_bytes()).unwrap().to_string())
+    }
+}
+
+pub fn read_line() -> Option<String> {
+    if cfg!(unix) {
+        read_line_unix("saira> ")
+    } else {
+        let mut s = "".to_string();
+        print!("saira>");
+        match stdin().read_line(&mut s) {
+            Ok(_) => {
+                s.pop();  // pop '\n'
+                Some(s)
+            }
+            Err(_) => None
+        }
+    }
+}
+
+
+
